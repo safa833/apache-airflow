524911735303f20718a108d3f9904dabc57fc82a
safa833 <safa833@gmail.com>
2025-03-04 14:12:18 +0300

    v1

---
+++ dags/1_image_download.py
@@ -0,0 +1,39 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime, timedelta
+import requests
+
+# Fonksiyon: Resmi indir ve kaydet
+def download_image():
+    url = "http://host.docker.internal:8081/arrow.png"
+    save_path = "/opt/airflow/data/arrow.png"
+    
+    response = requests.get(url)
+    if response.status_code == 200:
+        with open(save_path, "wb") as f:
+            f.write(response.content)
+    else:
+        raise Exception(f"Failed to download image, status code: {response.status_code}")
+
+# DAG tanımı
+default_args = {
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 3, 3),
+    "retries": 1,
+    "retry_delay": timedelta(seconds=10),
+}
+
+dag = DAG(
+    "1_download_arrow_image",
+    default_args=default_args,
+    description="Periodically download an image every minute",
+    schedule_interval=timedelta(minutes=1),
+    catchup=False,
+)
+
+download_task = PythonOperator(
+    task_id="download_image",
+    python_callable=download_image,
+    dag=dag,
+)
---
+++ dags/2_import_gpkg.py
@@ -0,0 +1,47 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+# from airflow.providers.postgres.operators.postgres import PostgresOperator
+from datetime import datetime
+import os
+
+DATA_DIR = "/opt/airflow/data"
+GPKG_URL = "http://host.docker.internal:8081/kontur_population_TR_20231101.gpkg.gz"
+GPKG_FILE = "kontur_population_TR_20231101.gpkg"
+GZ_FILE = f"{GPKG_FILE}.gz"
+DB_CONNECTION = "host=postgres user=airflow password=airflow dbname=airflow"
+
+default_args = {
+    "owner": "airflow",
+    "start_date": datetime(2024, 3, 4),
+    "retries": 1,
+}
+
+dag = DAG(
+    "2_import_gpkg",
+    default_args=default_args,
+    schedule_interval=None,
+    catchup=False
+)
+
+download_gpkg = BashOperator(
+    task_id="download_gpkg",
+    bash_command=f"wget -O {DATA_DIR}/{GZ_FILE} {GPKG_URL}",
+    dag=dag,
+)
+
+extract_gpkg = BashOperator(
+    task_id="extract_gpkg",
+    bash_command=f"gunzip -c {DATA_DIR}/{GZ_FILE} > {DATA_DIR}/{GPKG_FILE}",
+    dag=dag,
+)
+
+import_gpkg = BashOperator(
+    task_id="import_gpkg",
+    bash_command=(
+        f"ogr2ogr -f PostgreSQL PG:\"{DB_CONNECTION}\" {DATA_DIR}/{GPKG_FILE} "
+        "-progress --config PG_USE_COPY YES"
+    ),
+    dag=dag,
+)
+
+download_gpkg >> extract_gpkg >> import_gpkg
